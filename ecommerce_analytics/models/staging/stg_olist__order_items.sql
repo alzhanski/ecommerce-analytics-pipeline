5bd7bf3896487da6268e817cce519053eed1c129
alzhanski <alzhan1999@gmail.com>
2025-08-24 18:26:07 -0400

    removed the currency conversion

--- ecommerce_analytics/models/staging/stg_olist__order_items.sql
+++ ecommerce_analytics/models/staging/stg_olist__order_items.sql
@@ -11,12 +11,12 @@ order_items as (
     select 
 
         order_id,
-        order_item_id as num_items,
+        order_item_id,
         product_id,
         seller_id,
         shipping_limit_date as shipping_deadline_date,
-        round((price / 5.43)::numeric, 2) as price_us,
-        round((freight_value / 5.43)::numeric, 2) as freight_price_us
+        price,
+        freight_value
     
     from source
 )
