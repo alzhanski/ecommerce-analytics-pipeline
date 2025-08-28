ae2a742f611ee4dfff451c78cb2927fd3b75ce0e
alzhanski <alzhan1999@gmail.com>
2025-08-27 22:29:43 -0400

    updated field name

--- ecommerce_analytics/models/staging/stg_olist__customers.sql
+++ ecommerce_analytics/models/staging/stg_olist__customers.sql
@@ -11,7 +11,7 @@ customers as (
     select
 
         customer_unique_id,
-        customer_id as customer_order_id,
+        customer_id as order_customer_id,
         customer_zip_code_prefix as customer_zipcode,
         customer_city as customer_city,
         customer_state
